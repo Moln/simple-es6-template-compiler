f97472fbddbfb728202056c96b29415cb4632b92
Moln <moln.xie@gmail.com>
2019-02-21 15:40:56 +0800

    Fix "\r" str

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function simpleEs6Compiler(str, variables) {
     var reEsTemplate = /(\n)|(\')|\$\{([^\\}]*(?:\\.[^\\}]*)*)\}/g;
-    var importsKeys = ['__quote', '__ln'];
-    var importsValues = ['\'', '\n'];
+    var importsKeys = ['__quote', '__ln', '__r'];
+    var importsValues = ['\'', '\n', '\r'];
 
     for (var key in variables) {
         importsKeys.push(key);
